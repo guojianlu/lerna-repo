51a82b38c2cdf36d4e855adcadc3e7e7f32e09fa
guojianlu <jianluguo@yunify.com>
2021-07-27 22:30:31 +0800

    feat: add firstUpperCase feature
    affects: @ragingsnail/utils

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -6,3 +6,7 @@ exports.formatTime = (time, format = 'YYYY-MM-DD HH:mm:ss') => {
 exports.isObject = obj => {
   return obj && typeof obj === 'object' && obj !== null;
 };
+
+exports.firstUpperCase = str => {
+  return str.toLowerCase().replace(/( |^)[a-z]/g, L => L.toUpperCase());
+};
