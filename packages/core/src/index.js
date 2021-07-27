9c884581ab16f61e6068cf748bb855790ee72bb4
guojianlu <jianluguo@yunify.com>
2021-07-27 16:19:46 +0800

    add bar func

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -6,5 +6,9 @@ function sum(a, b) {
 function foo() {
   console.log('this is foo func');
 }
+
+function bar() {
+  console.log('this is bar func');
+}
 console.log('sum: ', sum(1, 2));
 console.log('当前时间是： ', Utils.formatTime(new Date()));
