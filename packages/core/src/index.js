7e717b94e22abeea6e6ffd17be46ee93ffb2e2ac
guojianlu <jianluguo@yunify.com>
2021-07-27 15:52:06 +0800

    add foo func

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -2,5 +2,9 @@ import Utils from '@ragingsnail/utils';
 function sum(a, b) {
   return a + b;
 }
+
+function foo() {
+  console.log('this is foo func');
+}
 console.log('sum: ', sum(1, 2));
 console.log('当前时间是： ', Utils.formatTime(new Date()));
