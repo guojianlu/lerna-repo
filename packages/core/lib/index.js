0492d0f5628ef280116f296cd6e55a388f9bf919
guojianlu <jianluguo@yunify.com>
2021-07-27 18:52:52 +0800

    feat: add a console log
    affects: @ragingsnail/core

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -6,8 +6,14 @@ function sum(a, b) {
   return a + b;
 }
 
+function square(n) {
+  return n * n;
+}
+
 console.log('sum: ', sum(1, 2));
+console.log('square: ', square(3));
 console.log('当前时间是： ', formatTime(new Date()));
 module.exports = {
-  sum
+  sum,
+  square
 };
--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -8,6 +8,7 @@ function square(n) {
 }
 
 console.log('sum: ', sum(1, 2));
+console.log('square: ', square(3))
 console.log('当前时间是： ', formatTime(new Date()));
 
 module.exports = { sum, square };
