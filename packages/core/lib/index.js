6b61cb417158eede641c41b376ebdb24601da8d1
guojianlu <jianluguo@yunify.com>
2021-07-27 22:24:27 +0800

    fix: add some gitignore items
    affects: @ragingsnail/core

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -6,12 +6,7 @@ function sum(a, b) {
   return a + b;
 }
 
-function square(n) {
-  return n * n;
-}
-
 console.log('sum: ', sum(1, 2));
-console.log('square: ', square(3));
 console.log('当前时间是： ', formatTime(new Date()));
 module.exports = {
   sum,
--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,14 +1,10 @@
 const { formatTime } = require('@ragingsnail/utils');
+
 function sum(a, b) {
   return a + b;
 }
 
-function square(n) {
-  return n * n;
-}
-
 console.log('sum: ', sum(1, 2));
-console.log('square: ', square(3))
 console.log('当前时间是： ', formatTime(new Date()));
 
 module.exports = { sum, square };
